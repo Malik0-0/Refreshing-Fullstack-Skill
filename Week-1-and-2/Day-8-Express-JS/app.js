cdb8e1f89bc5732598105f5e0cc3f97ecc0be8d5
Malik0-0 <malikalrasyidbasori.1@gmail.com>
2025-10-02 18:04:14 +0700

    day 8: Setup Express; Complete

---
+++ Week-1-and-2/Day-8-Express-JS/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+
+// View engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Routes
+const indexRouter = require("./routes/index");
+
+app.use("/", indexRouter);
+
+// Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
